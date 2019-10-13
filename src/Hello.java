16de9219ee606b13796d7d9a3b501211c5f865b7
wjlkkmac <wjlkkmac@outlook.com>
2019-10-13 12:16:22 +0800

    第一个分支提交，刚刚创建。。。。

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("w");
-        System.out.println("c");
+        System.out.println("wjl");
+
     }
 }
